e9aa45c3706024677e511e532c1adc886a4f0d74
Jonathan Adler <jonathana@wix.com>
2021-02-10 16:14:37 +0200

    lint

--- packages/wix-ui-tpa/src/components/AddItem/perf/AddItemExtended.tsx
+++ packages/wix-ui-tpa/src/components/AddItem/perf/AddItemExtended.tsx
@@ -3,5 +3,5 @@ import { AddItem } from '../';
 import { st, classes } from './AddItemExtended.st.css';
 
 export const ActionsMenuLayoutPerfer = () => {
-    return <AddItem className={st(classes.root)}>Add Item</AddItem>;
+  return <AddItem className={st(classes.root)}>Add Item</AddItem>;
 };
